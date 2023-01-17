82942fdfde7afd52a2efd8a5f1ced853490ec165
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2023-01-17 21:06:58 +0530

    Add files via upload

---
+++ MoodAnalyzer.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModeAnalyzerAssignment
+{
+    public class MoodAnalyzer
+    {
+        public static string analyseMood(string message)
+        {
+            
+            return message;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+namespace ModeAnalyzerAssignment
+{
+     public class Program
+     {
+        public static void Main(string[] args)
+        {
+            string message;
+            Random random= new Random();
+            int mood = random.Next(2);
+
+            if (mood == 1) 
+            {
+                 message = "Happy";
+            }
+            else
+            {
+                 message = "Sad";
+
+            }
+            Console.WriteLine(MoodAnalyzer.analyseMood(message));
+        }
+     }
+}
