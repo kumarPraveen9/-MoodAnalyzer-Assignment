ba14310f40a3f0ff1ace7e0fbb533f35029e6d70
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2023-01-17 21:18:35 +0530

    refactor code test case 1.1

---
+++ MoodAnalyzer.cs
@@ -0,0 +1,53 @@
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModeAnalyzerAssignment
+{
+    public class MoodAnalyzer
+    {
+        public string analyseMood(string message)
+        {
+            if (message == "I am in Sad Mood")
+            {
+                message = "SAD";
+                return message;
+
+            }
+            else
+            {
+                message = "HAPPY";
+                return message;
+            }
+        }
+
+    }
+    public class MoodAnalyser
+    {
+
+        public string msg;
+        public MoodAnalyser()
+        {
+            this.msg = "SAD";
+        }
+        public MoodAnalyser(string msg1)
+        {
+           this.msg = msg1;
+            
+        }
+        
+        public string analyseMood1()
+        {
+            MoodAnalyser obja = new MoodAnalyser();
+            return obja.msg;
+            
+        }
+
+    }
+
+}
+
