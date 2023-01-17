a1e458db15e1b0990c594194c510bb030d9eb807
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2023-01-17 22:17:27 +0530

    creating a class MoodAnalyzerNullException

---
+++ MoodAnalyzerNullException.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModeAnalyzerAssignment
+{
+  
+        public class MoodAnalyzerNullException : Exception
+        {
+            public MoodAnalyzerNullException(string msg) : base(msg)
+            {
+
+            }
+        }
+    
+}
