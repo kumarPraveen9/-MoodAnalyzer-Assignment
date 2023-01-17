8db56d4866e5fd2ea868ca7b23f435dd2e2af366
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2023-01-17 21:14:08 +0530

    test case1.1  for uc1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+namespace ModeAnalyzerAssignment
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string message = "I am in Sad Mood";
+            string message1 = "I am in Any Mood";
+            MoodAnalyzer moodAnalyzer1 = new MoodAnalyzer();
+            moodAnalyzer1.analyseMood(message);
+
+        }
+    }
+}
+
+
---
+++ TestCase1_1.cs
@@ -0,0 +1,25 @@
+using ModeAnalyzerAssignment;
+using System.Diagnostics.SymbolStore;
+
+namespace TestCaseMoodAnalyzer
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestCase1_1()
+        {
+            string input="I am in Sad Mood";
+            string expected = "SAD";
+
+
+            MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+            
+          string actual= moodAnalyzer.analyseMood(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        
+    }
+}
