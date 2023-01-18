0dad2f32543e014634e512919468d377d9134db3
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2023-01-18 19:53:17 +0530

    test case 5.2 for uc5

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -18,6 +18,19 @@ namespace TestCaseMoodAnalyzer
             expected.Equals(returned);
         }
 
+        [TestMethod]
+        public void UseCase5_2()
+        {
+            try
+            {
+                
+                object returned = Mood_Aanalyzer_Factory.CreateMoodAnalyze_Parameter_Constructor("ModeAnalyzerAssignment.MoodAnaler", "MoodAnalyser");
+            }
+            catch(MoodAnalyzerNullException Exception)
+            {
+                Assert.AreEqual("Class not found",Exception.Message);
+            }
+        }
      
 
     }
