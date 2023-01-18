99c221b8f21d0bf97df62b1caba41eeff7d06693
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2023-01-18 18:59:07 +0530

    testing for uc4.2

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -17,6 +17,17 @@ namespace TestCaseMoodAnalyzer
             expected.Equals(returned);
         }
       
+          [TestMethod]
+        public void UseCase4_2()
+        {
+            try
+            {
+                object returned = Mood_Aanalyzer_Factory.CreateMoodAnalyze("ModeAnalyzerAssignment.Mood", "Mood");
+            }
+            catch (MoodAnalyzerNullException Exception)
+            {
+                Assert.AreEqual("Class not found", Exception.Message);
+            }
         }
     }
 }
